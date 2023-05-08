86350e3e6606667608235957363ea834e955fd35
Matthew Whalen <matthewwhalen@Matthews-MacBook-Pro.local>
2023-05-08 01:18:12 -0500

    added initial pipeline obj

---
+++ R/pipeline.R
@@ -0,0 +1,48 @@
+#' Initialize a sewage Pipeline
+#' @export
+#' @returns A sewage pipeline object
+Pipeline = function() {
+  out = init_pipeline()
+  return(out)
+}
+
+init_pipeline = function() {
+  pipeline = list(
+    initialized = Sys.time(),
+    nodes = list(),
+    outputs = list()
+  )
+  structure(pipeline, class = "sewage_pipeline")
+}
+
+is_pipeline = function(x) {
+  inherits(x, 'sewage_pipeline')
+}
+
+' Run a pipeline
+#'
+#' This function is the extry point for executing a pipeline object
+#' @param pipeline an initialized pipeline object
+#' @param ... parameter(s) to pass to first node of the pipeline. This should match the `input` parameter of `add_node` of the first node.
+#' In the case that you have multiple inputs, each argument should match the name of a starting node in your pipeline.
+#' @export
+run = function(pipeline, ...) {
+
+  if(!is_pipeline(pipeline)) {
+    stop("pipeline object must be of type 'sewage_pipeline'")
+  }
+
+  dots = list(...)
+
+  pipeline[['outputs']] = dots
+  names(pipeline$outputs) = names(dots)
+
+  nodes = pipeline$nodes
+
+  for(node in nodes) {
+    pipeline = execute(node)
+  }
+
+  return(pipeline)
+
+}
---
+++ tests/testthat/test-pipeline.R
@@ -0,0 +1,10 @@
+test_that("pipeline creates pipeline object", {
+  pipeline = Pipeline()
+  expect_s3_class(pipeline, "sewage_pipeline")
+})
+
+
+test_that("run fails for non-pipelines", {
+  pipeline = list()
+  expect_error(run(pipeline, file = 'temp.csv'), regexp = "pipeline object must be of type 'sewage_pipeline'")
+})
