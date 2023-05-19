69389efd11981a1cc74707753cd25a0cc6f633ee
Matthew Whalen <matthewwhalen@Matthews-MacBook-Pro.local>
2023-05-19 09:02:30 -0500

    updated readme for pull_output

--- R/pipeline.R
+++ R/pipeline.R
@@ -19,7 +19,7 @@ is_pipeline = function(x) {
   inherits(x, 'sewage_pipeline')
 }
 
-' Run a pipeline
+#' Run a pipeline
 #'
 #' This function is the extry point for executing a pipeline object
 #' @param pipeline an initialized pipeline object
@@ -55,13 +55,30 @@ is_executed_pipeline = function(x) {
   return(length(x$outputs) > 0)
 }
 
+#' Extract output components from a pipeline
+#' @param x an executed pipeline object
+#' @param component a character string specifying which output component to pull
+#' #' \dontrun{
+#' pipeline = Pipeline()
+#' pipeline = pipeline |>
+#'     add_node(name = 'Splitter', component = Splitter(), input = 'file')
+#' result = run(pipeline, file = mtcars)
+#' result$outputs$Splitter.output_1
+#' result$outputs$Splitter.output_2
+#' }
+#' @export
 pull_output = function(x, component, ...) {
   UseMethod("pull_output")
 }
 
+#' @export
+#' @rdname pull_output
 pull_output.sewage_pipeline = function(x, component, ...) {
   if(!is_executed_pipeline(x)) {
     stop("No outputs available. Please execute pipeline using 'run'")
+  }
+  if(!is.character(component)) {
+    stop("component must be a character string")
   }
   output = x$outputs[[component]]
   print(output)
